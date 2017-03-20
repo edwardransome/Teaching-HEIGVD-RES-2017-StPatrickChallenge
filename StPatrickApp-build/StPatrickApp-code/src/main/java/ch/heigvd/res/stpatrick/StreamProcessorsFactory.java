5d57299a37f84bec6d75a218592c7f6a27b06e7f
Edward Ransome <edward.ransome@heig-vd.ch>
2017-03-20 11:04:43 +0000

    Passed second test

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
@@ -1,20 +1,46 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
 /**
  * This class is responsible for providing different types of Stream Processors.
- * 
+ *
  * @author Olivier Liechti
  */
 public class StreamProcessorsFactory implements IStreamProcessorsFactory {
 
-  @Override
-  public IStreamProcessor getProcessor() {
-    return new BasicStreamProcessor();
-  }
+    @Override
+    public IStreamProcessor getProcessor() {
+        return new BasicStreamProcessor();
+    }
 
-  @Override
-  public IStreamProcessor getProcessor(String processorName) throws UnknownNameException {
-    throw new UnknownNameException("The factory does not know any processor called " + processorName);
-  }
+    @Override
+    public IStreamProcessor getProcessor(String processorName) throws UnknownNameException {
+        switch (processorName) {
+            case "e-remover":
+                return new IStreamProcessor() {
+                    public void process(Reader in, Writer out) throws IOException {
+                        BufferedReader br = new BufferedReader(in);
+                        BufferedWriter bw = new BufferedWriter(out);
+                        int c = br.read();
+                        while (c != -1) {
+                            if((char)c != 'e' && (char)c != 'E'){
+                                bw.write(c);
+                            }
+                            c = br.read();
+                        }
+                        bw.flush();
+                    }
+                };
+            default:
+
+        }
+        throw new UnknownNameException("The factory does not know any processor called " + processorName);
+
+    }
 
 }
